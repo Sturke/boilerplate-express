b562c6ede7dded4f807fd8849d74004c89c0eb99
Burke Stanion <burke.stanion@gmail.com>
2024-04-20 16:55:16 +0000

    Modify app and json

--- myApp.js
+++ myApp.js
@@ -2,8 +2,21 @@ let express = require('express');
 let app = express(
     
 );
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get("Hello Express");
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
+
+  app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
+  });
+
+  console.log('Print')
+
+
 
 
 
